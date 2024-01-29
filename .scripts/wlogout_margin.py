dcd1c772597ca7bbc438f0c43662ba9566d5c0a3
xiyori <foma@shipilov.ru>
2024-01-29 18:41:29 +0300

    Bug fixes and improvements

--- .scripts/wlogout_margin.py
+++ .scripts/wlogout_margin.py
@@ -6,6 +6,6 @@ import sys
 info = json.load(sys.stdin)
 for monitor in info:
     if monitor["focused"]:
-        print(int(monitor["height"] * 0.3))
+        print(int(monitor["height"] / monitor["scale"] * 0.37))
         exit(0)
 print(300)
