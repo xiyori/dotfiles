1a3c934d7578a3138c301baf6b7b8c090b83c344
xiyori <foma@shipilov.ru>
2024-02-27 11:21:44 +0300

    Multiple monitor config, fixes

--- .scripts/cmus_status.sh
+++ .scripts/cmus_status.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 STATE_DIR="$HOME/.local/state/cmus_status"
-COVERS_DIR="$STATE_DIR/cover"
+COVER_DIR="$STATE_DIR/cover"
+COVER_PATH="$COVER_DIR/cmus_status_cover.jpg"
 
 status=$2
 file_path=$(echo "$@" | grep -o "file .* artist")
@@ -10,15 +11,15 @@ file_path="${file_path% artist}"
 
 if [ "$file_path" != "" ]; then
     file_dir="$(dirname "${file_path}")"
-    img_name="$(ls -S1 "${file_dir}" | grep -oiE ".*(cover|folder|artwork|albumart).*\.(jpg|jpeg|png|tiff)" | head -1)"
-    if ! cp "${file_dir}/${img_name}" "$COVERS_DIR/cover.jpg" ; then
+    img_name="$(ls -S1 "${file_dir}" | grep -oiE ".*(cover|folder|artwork|albumart|jacket).*\.(jpg|jpeg|png|tiff)" | head -1)"
+    if ! cp "${file_dir}/${img_name}" "$COVER_PATH" ; then
         ffmpeg -i "${file_path}" -vframes 1 -update true -an "$STATE_DIR/cover.jpg"
         if [ -f "$STATE_DIR/cover.jpg" ]; then
-            mv "$STATE_DIR/cover.jpg" "$COVERS_DIR/cover.jpg"
+            mv "$STATE_DIR/cover.jpg" "$COVER_PATH"
         else
-            cp "$STATE_DIR/placeholder.png" "$COVERS_DIR/cover.jpg"
+            cp "$STATE_DIR/placeholder.png" "$COVER_PATH"
         fi
     fi
 else
-    cp "$STATE_DIR/placeholder.png" "$COVERS_DIR/cover.jpg"
+    cp "$STATE_DIR/placeholder.png" "$COVER_PATH"
 fi
--- .scripts/switch_out_from_empty.sh
+++ .scripts/switch_out_from_empty.sh
@@ -3,9 +3,9 @@
 function handle {
     if [[ ${1:0:11} == "closewindow" ]]; then
         echo "Close Window detected"
-        if hyprctl workspaces | grep "windows: 0"; then
+        if hyprctl activeworkspace | grep "windows: 0"; then
             echo "Empty workspace detected"
-            hyprctl dispatch workspace e-1
+            hyprctl dispatch workspace m-1
             # sleep 0.0001
             # hyprctl dispatch workspace 1
         fi
