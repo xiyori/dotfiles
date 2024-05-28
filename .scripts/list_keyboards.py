0e9af4bb15563041a89cd802aea233a21702e6a6
xiyori <foma@shipilov.ru>
2024-05-28 18:47:45 +0300

    Change keyboard layout logic

--- .scripts/list_keyboards.py
+++ .scripts/list_keyboards.py
@@ -3,12 +3,9 @@
 import json
 import sys
 
-omit = ["video-bus", "power-button", "sleep-button", "system-control", "consumer-control", "hotkeys", "moondrop", "mouse", "keyboard"]
+# omit = ["video-bus", "power-button", "sleep-button", "system-control", "consumer-control", "hotkeys", "moondrop", "mouse", "keyboard"]
 
 devices = json.load(sys.stdin)
 for keyboard in devices["keyboards"]:
-    for keyword in omit:
-        if keyword in keyboard["name"]:
-            break
-    else:
+    if keyboard["main"]:
         print(keyboard["name"])
