326cc2bebb2f280cb582c8d80d1bba3443f79194
xiyori <foma@shipilov.ru>
2024-06-05 14:24:08 +0300

    Change clock color

--- .scripts/clock.sh
+++ .scripts/clock.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-alacritty --class "tty-flex,tty-flex" -o "window.dimensions.lines=10" -o "window.dimensions.columns=60" -o "font.size=10" -e tty-clock -C 7 -c -s -D -d 0 -a 50000000
+alacritty --class "tty-flex,tty-flex" -o "window.dimensions.lines=10" -o "window.dimensions.columns=60" -o "font.size=10" -e tty-clock -C 5 -c -s -D -d 0 -a 50000000
