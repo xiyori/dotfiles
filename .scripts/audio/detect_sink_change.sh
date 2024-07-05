8c0cffffe04165401863cbbe22b5bf21dfaade29
xiyori <foma@shipilov.ru>
2024-07-05 09:49:48 +0300

    Add Blessing 3 profile

--- .scripts/audio/detect_sink_change.sh
+++ .scripts/audio/detect_sink_change.sh
@@ -2,8 +2,8 @@
 
 pw-link -l | grep "|<- LSP Loudness Compensator Stereo:Output" > /dev/null && exit 0
 
-for sink in $(~/.scripts/audio/list_sinks.sh) ; do
-    for priority_sink in $(cat ~/.config/myeffects/profiles.txt | cut -f 1) ; do
+for priority_sink in $(cat ~/.config/myeffects/profiles.txt | cut -f 1) ; do
+    for sink in $(~/.scripts/audio/list_sinks.sh) ; do
         if [ "$sink" == "$priority_sink" ]; then
             ~/.scripts/audio/set_active_sink.sh "$sink"
             exit 0
--- .scripts/launch_cmus.sh
+++ .scripts/launch_cmus.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if ! pgrep -f "alacritty -e cmus" > /dev/null 2>&1 ; then
     nohup alacritty -e cmus >/dev/null 2>&1 &
-    nohup ristretto ~/.local/state/cmus_status/cover >/dev/null 2>&1 &
     sleep 0.5
+    nohup ristretto ~/.local/state/cmus_status/cover >/dev/null 2>&1 &
 fi
 if [ "$#" -eq 1 ]; then
     cmus-remote -f "$1"
