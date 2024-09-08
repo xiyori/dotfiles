90e6b23b65fa7efaeb8c04ee71574482a2c8db72
xiyori <foma@shipilov.ru>
2024-09-08 20:35:20 +0300

    Fix Hyprland socket script

--- .scripts/switch_out_from_empty.sh
+++ .scripts/switch_out_from_empty.sh
@@ -45,4 +45,4 @@ function handle {
     fi
 }
 
-socat -t0 -,ignoreeof "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
+socat STDOUT "UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" | while read -r line; do handle "$line"; done
