da78af29306193db41f71c364308e853f237f550
xiyori <foma@shipilov.ru>
2024-09-17 16:23:07 +0300

    Update cava script

--- .scripts/cava.sh
+++ .scripts/cava.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-alacritty --class "tty-flex,tty-flex" -o "window.dimensions.lines=16" -o "window.dimensions.columns=91" -o "font.size=8" -e cava
+alacritty --class "cava,cava" -o "font.size=8" -e cava
