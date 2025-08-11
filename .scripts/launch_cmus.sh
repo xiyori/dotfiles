78019b2487d0ff6c5c5c79967dd461d6822db62d
xiyori <foma@shipilov.ru>
2025-08-12 00:03:37 +0300

    Seamless cmus launch

--- .scripts/launch_cmus.sh
+++ .scripts/launch_cmus.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-function wait_open {
-    while ! hyprctl activewindow | grep "class: $1" > /dev/null 2>&1 ; do
-        sleep 0.1
-    done
-}
+# function wait_open {
+#     pgrep "$1"
+#     while ! pgrep "$1" > /dev/null 2>&1 ; do
+#         sleep 0.1
+#     done
+# }
 
 if ! pgrep -f "alacritty --class cmus,cmus" > /dev/null 2>&1 ; then
     mkdir -p /tmp/cmus_status
     cp ~/.local/state/cmus_status/placeholder.png /tmp/cmus_status/cmus_status_cover.jpg
     alacritty --class "cmus,cmus" -e cmus >/dev/null 2>&1 &
-    sleep 0.5
+    sleep 0.1
     alacritty --class "cmus-cava,cmus-cava" -o "font.size=8" -e cava >/dev/null 2>&1 &
-    sleep 0.2
-    hyprctl dispatch splitratio 0.195
+    sleep 0.1
     qimgv /tmp/cmus_status/cmus_status_cover.jpg >/dev/null 2>&1 &
-    sleep 0.2
+    sleep 0.1
     hyprctl dispatch splitratio -0.5
+    sleep 0.1
     hyprctl dispatch focuswindow 'class:^(cmus)$'
+    hyprctl dispatch splitratio 0.195
 fi
 if [[ "$#" -eq 1 ]]; then
     cmus-remote -f "$1"
