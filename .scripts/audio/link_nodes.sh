d2b2b7f45c8354e61c48e55a141d063fa4f39190
xiyori <foma@shipilov.ru>
2026-04-26 22:37:27 +0300

    More robust midi linking

--- .scripts/audio/link_nodes.sh
+++ .scripts/audio/link_nodes.sh
@@ -2,10 +2,11 @@
 
 output_node="LSP Loudness Compensator Stereo"
 midi_device="$(~/.scripts/audio/midi_device.sh | cut -d ":" -f 2 | tr "," "-")"
-midi_bridge="Midi-Bridge:Virtual Raw MIDI ${midi_device}VirMIDI ${midi_device}"
+midi_out="$(pw-link -Iol "Midi-Bridge" | grep "$midi_device" | xargs | cut -d " " -f 2-)"
+midi_in="$(pw-link -Iil "" "Midi-Bridge" | grep "$midi_device" | xargs | cut -d " " -f 2-)"
 
-pw-link "${midi_bridge} (capture)" "${output_node}:events-in"
-pw-link "${output_node}:events-out" "${midi_bridge} (playback)"
+pw-link "$midi_out" "${output_node}:events-in"
+pw-link "${output_node}:events-out" "$midi_in"
 
 if [[ -z "$1" ]]; then
     pw-link "myeffects_sink:monitor_FL" "${output_node}:Input L"
